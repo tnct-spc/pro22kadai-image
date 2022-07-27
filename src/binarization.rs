73db74889add1b0cc6bf9aa5cef93fa41f42e0d9
SuperYuro <a.gpgtdmgp@gmail.com>
2022-07-27 10:57:39 +0900

    [fix]fix converting 2d vec and 1d vec

--- src/binarization.rs
+++ src/binarization.rs
@@ -1,19 +1,19 @@
 // Vec[y][x]
 
-fn conv_to_line(img: &Vec<Vec<u8>>) -> Vec<u8> {
+pub fn conv_to_line(img: &Vec<Vec<u8>>) -> Vec<u8> {
     let mut ret = Vec::new();
     let x_max = img[0].len();
     let y_max = img.len();
 
     for y in 0..y_max {
         for x in 0..x_max {
-            ret[x + x_max * y] = img[y][x];
+            ret.push(img[y][x]);
         }
     }
     ret
 }
 
-fn conv_from_line(img: &Vec<u8>, x_max: usize) -> Vec<Vec<u8>> {
+pub fn conv_from_line(img: &Vec<u8>, x_max: usize) -> Vec<Vec<u8>> {
     let mut ret = Vec::new();
     let y_max = img.len() / x_max;
 
@@ -28,7 +28,7 @@ fn conv_from_line(img: &Vec<u8>, x_max: usize) -> Vec<Vec<u8>> {
 }
 
 // threshold: 輝度のしきい値（これ以上は白画素，これ以下は黒画素）
-fn binarize(img: &mut Vec<Vec<u8>>, threshold: usize) {
+pub fn binarize(img: &mut Vec<Vec<u8>>, threshold: usize) {
     let mut p_src = conv_to_line(img);
     let x_max = img[0].len();
     let y_max = img.len();
@@ -41,4 +41,19 @@ fn binarize(img: &mut Vec<Vec<u8>>, threshold: usize) {
     for i in 0..x_max * y_max {
         img[i / x_max][i % x_max] = lut[p_src[i] as usize];
     }
+}
+
+// Pタイル法の閾値を求める
+pub fn get_threhsold(img: &mut Vec<Vec<u8>>) -> u8 {
+    let mut histgram = [0; 256];
+
+    let x_max = img[0].len();
+    let y_max = img.len();
+
+    for y in 0..y_max {
+        for x in 0..x_max {
+            histgram[img[y][x] as usize] += 1;
+        }
+    }
+    0
 }
