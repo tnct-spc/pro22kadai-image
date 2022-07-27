3b0d378654ea04c8677c59a390e7ec414b59393d
SuperYuro <a.gpgtdmgp@gmail.com>
2022-07-27 17:07:24 +0900

    [fix]add error processing

--- src/binarization.rs
+++ src/binarization.rs
@@ -17,19 +17,22 @@ pub fn conv_from_line(img: &Vec<u8>, x_max: usize) -> Vec<Vec<u8>> {
     let mut ret = Vec::new();
     let y_max = img.len() / x_max;
 
-    for y in 0..y_max {
-        let mut ret_x = Vec::new();
-        for x in 0..x_max {
-            ret_x.push(img[y * x_max + x]);
+    if x_max * y_max == img.len() {
+        for y in 0..y_max {
+            let mut ret_x = Vec::new();
+            for x in 0..x_max {
+                ret_x.push(img[y * x_max + x]);
+            }
+            ret.push(ret_x);
         }
-        ret.push(ret_x);
+        return ret;
     }
-    ret
+    vec![vec![0]]
 }
 
 // threshold: 輝度のしきい値（これ以上は白画素，これ以下は黒画素）
 pub fn binarize(img: &mut Vec<Vec<u8>>, threshold: usize) {
-    let mut p_src = conv_to_line(img);
+    let p_src = conv_to_line(img);
     let x_max = img[0].len();
     let y_max = img.len();
 
