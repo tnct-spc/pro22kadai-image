2759c943dd65967f53808f606e38834f36d79c36
SuperYuro <a.gpgtdmgp@gmail.com>
2022-08-12 13:25:28 +0900

    Create png_reader.rs
    ライブラリを使用してPNGを読んでRGBAそれぞれの画素の輝度値を返す関数が入っているファイルを追加

---
+++ src/png_reader.rs
@@ -0,0 +1,42 @@
+use std::os::windows;
+
+use image::GenericImageView;
+
+pub fn get_pixel_data(
+    filename: &str,
+) -> (
+    Vec<Vec<usize>>,
+    Vec<Vec<usize>>,
+    Vec<Vec<usize>>,
+    Vec<Vec<usize>>,
+) {
+    let img = image::open(filename).unwrap();
+
+    let image_width = img.width();
+    let image_height = img.height();
+
+    let mut red_pixels = Vec::new();
+    let mut green_pixels = Vec::new();
+    let mut blue_pixels = Vec::new();
+    let mut alpha_pixels = Vec::new();
+
+    for y in 0..image_height {
+        let mut red_line = Vec::new();
+        let mut green_line = Vec::new();
+        let mut blue_line = Vec::new();
+        let mut alpha_line = Vec::new();
+
+        for x in 0..image_width {
+            let d = img.get_pixel(x, y);
+            red_line.push(d[0] as usize);
+            green_line.push(d[1] as usize);
+            blue_line.push(d[2] as usize);
+            alpha_line.push(d[3] as usize);
+        }
+        red_pixels.push(red_line);
+        green_pixels.push(green_line);
+        blue_pixels.push(blue_line);
+        alpha_pixels.push(alpha_line);
+    }
+    (red_pixels, green_pixels, blue_pixels, alpha_pixels)
+}
