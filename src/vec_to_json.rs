94f1dccea290305130b997b3facb3cf1ff13d7d9
SuperYuro <a.gpgtdmgp@gmail.com>
2022-08-15 10:07:56 +0900

    Create vec_to_json.rs
    座標配列をJsonに書き出すやつを書いた

---
+++ src/vec_to_json.rs
@@ -0,0 +1,17 @@
+use crate::corner_detector::Coordinate;
+
+/* Format Sample
+{ "coordinates": [ { "x": 12, "y": 128 }, { "x": 12, "y": 128 }, ] }
+ */
+
+pub fn vec_to_json(points: &Vec<Coordinate>) -> String {
+    let mut ret = String::new();
+
+    ret.push_str("{\"coordinates\":[");
+
+    for p in points {
+        ret.push_str(format!("{{\"x\":{},\"y\":{}}},", (*p).x, (*p).y).as_str());
+    }
+    ret.push_str("]}");
+    ret
+}
