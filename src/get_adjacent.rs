4e7306cc000e3405d3e7b8a31bebaff189abfff1
SuperYuro <a.gpgtdmgp@gmail.com>
2022-08-21 12:42:49 +0900

    Update get_adjacent.rs

--- src/get_adjacent.rs
+++ src/get_adjacent.rs
@@ -1,5 +1,17 @@
 use crate::corner_detector::Coordinate;
 
+enum Orthant {
+    Up,
+    LeftUp,
+    Left,
+    LeftDown,
+    Down,
+    RightDown,
+    Right,
+    RightUp,
+    Zero,
+}
+
 pub fn get_adjacent_matrix(img: &Vec<Vec<usize>>, points: &Vec<Coordinate>) -> Vec<Vec<usize>> {
     let s = points.len();
 
@@ -8,38 +20,18 @@ pub fn get_adjacent_matrix(img: &Vec<Vec<usize>>, points: &Vec<Coordinate>) -> V
     for j in 0..s {
         for i in j..s {
             ret[i][j] = is_adjacent(img, &points[i], &points[j]);
+            ret[j][i] = is_adjacent(img, &points[i], &points[j]);
         }
     }
     ret
 }
 
-// とりあえず分岐点はないものとする（つまり，1つの特徴点から伸びている辺は2本だけとする）
 // 点どうしが辺でつながっているかどうかを調べる．つながっていた場合はコストを返す，つながっていなかった場合は0を返す
 fn is_adjacent(img: &Vec<Vec<usize>>, start: &Coordinate, goal: &Coordinate) -> usize {
-    let x = start.x;
-    let y = start.y;
+    let mut current = start;
 
-    // Get branches
-    for j in 0..3 {
-        for i in 0..3 {
-            if i != 1 && j != 1 {
-                if img[j][i] == 1 {
-                    let cost = is_adjacent(
-                        img,
-                        &Coordinate {
-                            x: x + i - 1,
-                            y: y + j - 1,
-                        },
-                        goal,
-                    );
-                    if cost == 0 {
-                        return 0;
-                    } else {
-                        return cost + 1;
-                    }
-                }
-            }
-        }
-    }
+    let mut past_coord = Coordinate::new();
+
+    loop {}
     0
 }
