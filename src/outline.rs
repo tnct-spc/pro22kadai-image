96ed693586df4d885f5e131e1f5728e11276fdb6
SuperYuro <a.gpgtdmgp@gmail.com>
2022-08-27 22:42:47 +0900

    Update outline.rs
    ラベリング処理（製作途中）を追加

--- src/outline.rs
+++ src/outline.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefMut, cmp::min};
+
 // outline：輪郭線を抽出する
 // img：2値化した配列（黒画素：0，白画素：1）
 pub fn outline(img: &mut Vec<Vec<usize>>) {
@@ -37,4 +39,46 @@ fn zero_padding(img: &mut Vec<Vec<usize>>) {
         img[0][x] = 0;
         img[y_max - 1][x] = 0;
     }
+}
+
+pub fn labelling(img: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let y_max = img.len();
+    let x_max = img[0].len();
+
+    let mut ret = vec![vec![0; x_max]; y_max];
+
+    let mut lut = vec![0];
+
+    for y in 1..y_max - 1 {
+        for x in 1..x_max - 1 {
+            if img[y][x] == 1 {
+                // 白画素だった場合
+                if img[y][x - 1] + img[y - 1][x - 1] + img[y - 1][x] + img[y - 1][x + 1] == 0 {
+                    // 左，左上，上，右上の画素がすべて0の場合
+                    // 最後に割り振った番号+1（=lutの要素数）を割り振る
+                    ret[y][x] = lut.len();
+                    lut.push(lut.len());
+                } else {
+                    // 左上，上，右上，左のどれかが0じゃない
+                    // 最小のラベルを探す
+                    let mut s = lut.len();
+                    if img[y][x - 1] > 0 {
+                        // 左側の画素が0じゃない場合，左側の画素のラベルを候補にする
+                        s = img[y][x - 1];
+                    }
+                    let j = y - 1;
+                    let i = x - 1;
+                    if img[j][i] > 0 {
+                        // 左上の画素が0じゃない場合
+                        if img[j][i] < s {
+                            // 左上の画素のラベルが候補より小さい場合，ラベル候補とlutを更新する
+
+                            s = img[j][i];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    ret
 }
