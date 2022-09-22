83614f3a0d16eb71c515f6955d5c7561f143f118
Tatsuaki5 <55takitaki@gmail.com>
2022-09-22 12:57:15 +0900

    update get_bace64_from_url.rs

--- src/get_bace64_from_url.rs
+++ src/get_bace64_from_url.rs
@@ -1,21 +1,41 @@
-// 手順
-// 1.urlを受け取る. 形式 : "https://aws/?img=base64データ"
-// 2."https://aws/?img=" を消去.
-// 3."-"を"+" に、 "_"を"/" に変換.
-// 4.(4 - 文字数を4で割ったあまり)個の"="を末尾に追加.
-// 5.文字列を返す.
+/*
+手順
+urlを受け取る. 形式 : "https://aws/?"..."img=base64データ"...
+"&"で分割
+"img="を検索
+ヒットした部分を取り出す
+"img=" を消去.
+"-"を"+" に、 "_"を"/" に変換.
+(4 - 文字数を4で割ったあまり)個の"="を末尾に追加.
+文字列を返す.
+*/
 
 pub fn get_bace64_from_url(url: &str) -> std::string::String {
-    let mut url2: String = url.replace("https://aws/?img=", "");
-    let mut url3: String = url2.replace("-", "+");
-    url2 = url3.replace("_", "/");
+  let mut url2: String = url.replace("https://aws/?", "");
 
-    let mut long;
-  	long = url2.chars().count();
-    long = 4-long;
-    let mut i = 0;
-    for i in 0..long {
-        url2.push('=');
+  //＆で分割してimg=が含まれる部分を割り出す
+  let v: Vec<String> = url2.split('&').fold(Vec::new(), |mut s, i| {
+    s.push(i.to_string());
+    s
+  });
+  let mut j: usize = 0;
+  let mut flag: usize = 0;
+  while flag != 0 {
+    if v[j].find("img=") != None {
+      flag = j;
     }
+  }
+
+  url2 = v.get(flag).unwrap().to_string();
+
+  //各種置き換え
+  url2 = url2.replace("img=", "");
+  url2 = url2.replace("-", "+");
+  url2 = url2.replace("_", "/");
+  //文字数が4の倍数になるよう"="を追加
+  for i in 0..4-url2.chars().count() {
+      url2.push('=');
+  }
+
 	return url2;
 }
