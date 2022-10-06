1242e7b5a211df514f5c670cda7cec315167d2e6
SuperYuro <a.gpgtdmgp@gmail.com>
2022-10-07 01:51:18 +0900

    Response部分にそれっぽい実装をした

--- src/aws_socket.rs
+++ src/aws_socket.rs
@@ -1,4 +1,4 @@
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde_json::{json, Value};
 
 use crate::main;
@@ -13,5 +13,17 @@ async fn main() -> Result<(), Error> {
 async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let (_event, _context) = event.into_parts();
 
-    Ok(json!({"coordinates": [{"x": 128,"y": 16,},{"x": 12,"y": 163,},{"x": 12,"y": -163,},]}))
+    let encoded_img = get_base64_from_url(url);
+    let img = get_gray_data_from_base64(encoded_img);
+    let mut img = binarize(img);
+    outline(&mut img);
+    noize_erase(&mut img);
+
+    let points = pick_corner_point(&img);
+
+    let adjacent_matrix = get_adjacent_matrix(&points, &img);
+
+    let (points, adjacent_matrix) = merge_points(points, adjacent_matrix);
+
+    Ok(vec_to_json(points, adjacent_matrix))
 }
