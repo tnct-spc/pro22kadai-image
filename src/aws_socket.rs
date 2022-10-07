02c327fa8bf88a84098cf2aeebef61b625db56ac
SuperYuro <a.gpgtdmgp@gmail.com>
2022-10-07 16:14:44 +0900

    めっちゃチャレンジした

--- src/aws_socket.rs
+++ src/aws_socket.rs
@@ -1,29 +1,15 @@
-use lambda_runtime::{service_fn, Error, LambdaEvent};
+use lambda_runtime::{handler_fn, Context, Error};
 use serde_json::{json, Value};
 
-use crate::main;
+use crate::get_points;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = service_fn(func);
+    let func = handler_fn(func);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (_event, _context) = event.into_parts();
-
-    let encoded_img = get_base64_from_url(url);
-    let img = get_gray_data_from_base64(encoded_img);
-    let mut img = binarize(img);
-    outline(&mut img);
-    noize_erase(&mut img);
-
-    let points = pick_corner_point(&img);
-
-    let adjacent_matrix = get_adjacent_matrix(&points, &img);
-
-    let (points, adjacent_matrix) = merge_points(points, adjacent_matrix);
-
-    Ok(vec_to_json(points, adjacent_matrix))
+async fn func(event: Value, context: Context) -> Result<Value, Error> {
+    Ok(event)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use serde_json::Value;
 
+mod binarization;
+mod coordinate;
+mod corner_detector;
+mod get_adjacent;
+// mod labelling;
+mod aws_socket;
+mod merge_points;
+mod outline;
+mod png_reader;
+mod print;
+mod vec_to_json;
+
 use binarization::binarize;
 use coordinate::Coordinate;
 use corner_detector::{noize_erase, pick_corner_point, print_coordinates};
@@ -14,18 +26,6 @@ use print::print_adjacent_points;
 use print::print_points;
 use print::{print_adjacent_matrix, print_ptn, print_vec};
 use vec_to_json::vec_to_json;
-
-mod binarization;
-mod coordinate;
-mod corner_detector;
-mod get_adjacent;
-// mod labelling;
-// mod aws_socket;
-mod merge_points;
-mod outline;
-mod png_reader;
-mod print;
-mod vec_to_json;
 
 fn main() {
     let filename = "daruma_padd.png";
